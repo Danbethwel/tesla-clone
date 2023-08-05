791b85f5dac0f8ff3a6c773573bc9d8306f07ad6
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-08-05 10:34:34 +0300

    new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
+import React ,{ useState} from 'react'
+import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-function header() {
+
+function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
   return (
     <Container>
       <a>
@@ -17,9 +20,12 @@ function header() {
       <RightMenu>
         <a href=''>Shop</a>
         <a href=''>Tesla Account</a>
-        <CustomMenu/>
+        <CustomMenu onClick ={()=>setBurgerStatus(true)}/>
       </RightMenu>
-      <BurgerNav>
+      <BurgerNav show={burgerStatus}>
+        <CustomWrapper>
+          <CustomClose onClick={()=> setBurgerStatus(false)}/>
+        </CustomWrapper>
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
         <li><a href="#">Trade-In</a></li>
@@ -35,7 +41,7 @@ function header() {
   )
 }
 
-export default header
+export default Header;
 
 const Container = styled.div`
   min-height: 60px;
@@ -81,7 +87,7 @@ align-items: center;
 const CustomMenu= styled(MenuIcon)`
   cursor: pointer;
 
-`
+`;
 const BurgerNav = styled.div`
   position: fixed;
   top: 0;
@@ -89,5 +95,30 @@ const BurgerNav = styled.div`
   right: 0;
   background: white;
   width: 300px;
-  z-index: 100;
-`
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${props => props.show? 'translateX(0)': 'translateX(100%)'};
+  transition: transform 0.2s ;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+    a{
+       font-weight: 600;
+    }
+  }
+
+  
+`;
+const CustomClose = styled(CloseIcon)`
+   cursor: pointer;
+`;
+
+const CustomWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
